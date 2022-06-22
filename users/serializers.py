4f76e1ba3522bc0d602b074e431cf847ad2f87a7
MnrGDKL <munirgedikli.2021@gmail.com>
2022-06-22 22:50:43 +0300

    23

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,7 @@
-from asyncore import write
 from rest_framework import serializers, validators
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-
+from dj_rest_auth.serializers import TokenSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -40,3 +39,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = (
+      "username",
+      "first_name",
+      "last_name",
+      "email"
+    )
+
+class CustomTokenSerializer(TokenSerializer):
+  user = UserSerializer(read_only=True)
+  class Meta(TokenSerializer.Meta):
+    fields = (
+      "key",
+      "user"
+    )
